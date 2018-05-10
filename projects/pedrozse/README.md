532a838a2d7921e2799fc895d5ca86cbea98b72f
ballin22s <pedrozse@onid.oregonstate.edu>
2018-05-10 15:29:01 -0700

    Add changes to code

--- projects/pedrozse/README.md
+++ projects/pedrozse/README.md
@@ -1,4 +1,14 @@
 sergio pedroza
 pedrozse
 
-This is my random quiz submission submission!
+This is my assignment 4 submission!
+
+To get it working:
+make rngs.o
+make dominion.o
+make randomtestadventurer
+make randomtestcard1
+make randomtestcard2
+make randomtestadventurer.out
+make randomtestcard1.out
+make randomtestcard2.out
--- projects/pedrozse/dominion/unittest1.c
+++ projects/pedrozse/dominion/unittest1.c
@@ -1,91 +1,100 @@
 #include "dominion.h"
 #include "dominion_helpers.h"
-#include "rngs.h"
 #include <string.h>
 #include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
 #include <assert.h>
+#include "rngs.h"
 
+// global count of test failures
+int failureCount = 0;
 
-//unit test 1 tests existance of Copper, Silver and Gold Coins 
-int main (int argc, char** argv)	{
-	struct gameState G;
-	G.coins = 0;
-	
-	printf("\nUnit test 1: Coins\n");
-	
-	printf("\nCopper Test:\n");
-	G.hand[0][0] = copper;
-	G.hand[0][1] = copper;
-	G.hand[0][2] = copper;
-	G.hand[0][3] = copper;
-	G.hand[0][4] = copper;
-	G.hand[0][5] = copper;
-	G.hand[0][6] = copper;
-	G.hand[0][7] = copper;
-	G.hand[0][8] = copper;
-	G.hand[0][9] = copper;
-	updateCoins(0,&G, 0);
-	printf("\nWe should have 10 copper coins: %d\n", G.coins);
-	if(G.coins == 10)
-		printf("Test Passed\n");
-	else
-		printf("Test Failed\n");
-	
-	printf("\nSilver Test:\n");
-	G.hand[0][0] = silver;
-	G.hand[0][1] = silver;
-	G.hand[0][2] = silver;
-	G.hand[0][3] = silver;
-	G.hand[0][4] = silver;
-	G.hand[0][5] = silver;
-	G.hand[0][6] = silver;
-	G.hand[0][7] = silver;
-	G.hand[0][8] = silver;
-	G.hand[0][9] = silver;
-	updateCoins(0,&G, 0);
-	printf("We should have 10 silver coins: %d\n", G.coins);
-	if(G.coins == 20)
-		printf("Test Passed\n");
-	else
-		printf("Test Failed\n");
-	
-	printf("\nGold Test:\n");
-	G.hand[0][0] = gold;
-	G.hand[0][1] = gold;
-	G.hand[0][2] = gold;
-	G.hand[0][3] = gold;
-	G.hand[0][4] = gold;
-	G.hand[0][5] = gold;
-	G.hand[0][6] = gold;
-	G.hand[0][7] = gold;
-	G.hand[0][8] = gold;
-	G.hand[0][9] = gold;
-	updateCoins(0,&G, 0);
-	printf("We should have 10 gold coins: %d\n", G.coins);
-	if(G.coins == 30)
-		printf("Test Passed\n");
-	else
-		printf("Test Failed\n");
-	
-	printf("\nTest for zero existing Coin:\n");
-	G.hand[0][0] = adventurer;
-	G.hand[0][1] = adventurer;
-	G.hand[0][2] = adventurer;
-	G.hand[0][3] = adventurer;
-	G.hand[0][4] = adventurer;
-	G.hand[0][5] = adventurer;
-	G.hand[0][6] = adventurer;
-	G.hand[0][7] = adventurer;
-	G.hand[0][8] = adventurer;
-	G.hand[0][9] = adventurer;
-	updateCoins(0,&G, 0);
-	printf("We should have zero existing Coins: %d\n", G.coins);
-	if(G.coins == 0)
-		printf("Test Passed");
-	else
-		printf("Test Failed");
-	
-	return 0;
+// function to check if two ints are equal or not
+void assertTrue(int a, int b) {
+    if (a == b) {
+        printf("Test: PASSED\n");
+    }
+    else {
+        printf("Test: FAILED\n");
+        failureCount++;
+    }
+}
+
+// runs the tests
+int main () {
+    int i, b;
+    int numbPlayers = 2;
+    int player = 0;
+
+    // kingdom cards
+    int k[10] = {adventurer, council_room, feast, gardens, mine,
+               remodel, smithy, village, baron, great_hall};
+    int seed = 2000;
+    struct gameState state;
+    int handCount;
+    int bonus = 1;
+
+    int coppers[MAX_HAND];
+    int silvers[MAX_HAND];
+    int golds[MAX_HAND];
+
+    // arrays full of designated treasure
+    for (i = 0; i < MAX_HAND; i++) {
+        coppers[i] = copper;
+        silvers[i] = silver;
+        golds[i] = gold;
+    }
+
+    printf("Testing -> updateCoins()");
+
+    // test how updateCoins handles each treasure and a different bonus and hand count
+    for (handCount = 0; handCount <= 5; handCount = handCount + 5) {
+        printf("\n\nTreasure cards: %d\n",handCount);
+        printf("Bonus: %d\n", bonus);
+
+        memset(&state,23,sizeof(struct gameState));
+        b = initializeGame(numbPlayers, k, seed, &state);
+        state.handCount[player] = handCount;
+
+        // fill hand with all coppers
+        memcpy(state.hand[player],coppers,sizeof(int)*handCount);
+        updateCoins(player,&state,bonus);
+
+        printf("\nCoin Count - copper\n");
+        printf("Actual: %d\n",state.coins);
+        printf("Expected: %d\n",handCount*1+bonus);
+
+        assertTrue(state.coins,handCount*1+bonus);
+
+        // fill hand with all silvers
+        memcpy(state.hand[player],silvers,sizeof(int)*handCount);
+        updateCoins(player,&state,bonus);
+
+        printf("\nCoin Count - silver\n");
+        printf("Actual: %d\n",state.coins);
+        printf("Expected: %d\n",handCount*2+bonus);
+
+        assertTrue(state.coins,handCount*2+bonus);
+
+        // fill hand with all golds
+        memcpy(state.hand[player],golds,sizeof(int)*handCount);
+        updateCoins(player,&state,bonus);
+
+        printf("\nCoin Count - gold\n");
+        printf("Actual: %d\n",state.coins);
+        printf("Expected: %d\n",handCount*3+bonus);
+
+        assertTrue(state.coins,handCount*3+bonus);
+
+        bonus = bonus + 2;
+    }
+
+    if (failureCount) {
+        printf("\nTEST FAILED\n");
+        printf("Fails: %d\n",failureCount);
+    }
+    else {
+        printf("\nTEST SUCCESSFULLY COMPLETED\n\n");
+    }
+
+    return 0;
 }
